5e5475e826dce21e52d4b9775d7efb5b423de677
Ayubxon Axmatxonov <114375261+ayubxon1775@users.noreply.github.com>
2026-01-29 15:22:03 +0500

    veraibles

---
+++ python-math.py
@@ -0,0 +1,30 @@
+# Pythonda mashqlar, topshiriqlar va misollar
+
+#  1 "Hello World!" matnini yangi o'zgaruvchiga yuklang va print() yordamida konsolga chiqaring
+
+salom = 'Hello world'
+# print(salom)
+
+#  2 "xabar" deb nomlangan o'zgaruvchiga biror matn yuklang va konsolga chiqaring, keyin esa o'zgaruvchiga yangi qiymat berib uni ham konsolga chiqaring.
+
+xabar = 'Assalomu aleykum'
+# print(xabar)
+xabar = 'Hello world'
+# print(xabar)
+
+#  3'class' den nomlangan o'zgaruvchi yarating, unga biror qiymat bering va konsolga chiqaring (siz kutgan natija chiqdimi?)
+
+    # O'zgaruvchiga class deb nom berib bolmaydi chunki u maxsus kalit soz
+# class = 'salom'
+# print(class)
+
+#  4
+# radius = 5
+# pi = 3.14159
+# aylana_yuzi = pi * radius**2
+# print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
+
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
