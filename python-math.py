99bb24e52a61ce4890f607b43647707693b0ff7f
Ayubxon Axmatxonov <114375261+ayubxon1775@users.noreply.github.com>
2026-02-03 10:10:41 +0500

    sonlar

--- python-math.py
+++ python-math.py
@@ -3,14 +3,14 @@
 ################# O'zgaruvchilar
 #  1 "Hello World!" matnini yangi o'zgaruvchiga yuklang va print() yordamida konsolga chiqaring
 
-salom = 'Hello world'
+# salom = 'Hello world'
 # print(salom)
 
 #  2 "xabar" deb nomlangan o'zgaruvchiga biror matn yuklang va konsolga chiqaring, keyin esa o'zgaruvchiga yangi qiymat berib uni ham konsolga chiqaring.
 
-xabar = 'Assalomu aleykum'
+# xabar = 'Assalomu aleykum'
 # print(xabar)
-xabar = 'Hello world'
+# xabar = 'Hello world'
 # print(xabar)
 
 #  3'class' den nomlangan o'zgaruvchi yarating, unga biror qiymat bering va konsolga chiqaring (siz kutgan natija chiqdimi?)
@@ -25,12 +25,12 @@ xabar = 'Hello world'
 # aylana_yuzi = pi * radius**2
 # print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
 
-radius = 5
-pi = 3.14159
-aylana_yuzi = pi * radius**2
+# radius = 5
+# pi = 3.14159
+# aylana_yuzi = pi * radius**2
 # print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
 
-
+ 
 ############## String methodlar
 
 # Quyidagi o'zgaruvchilarni yarating: 
@@ -61,25 +61,46 @@ aylana_yuzi = pi * radius**2
 
 # Yuqoridagi matnni konsolga chiqarishda har bir verguldan keyin yangi qatordan yozing
 
-kocha = input("ko'chasini kiriting ")
-mahalla = input("mahallasini kiriting ")
-tuman = input("tumanini kiriting ")
-viloyat = input("viloyatini kiriting ")
+# kocha = input("ko'chasini kiriting ")
+# mahalla = input("mahallasini kiriting ")
+# tuman = input("tumanini kiriting ")
+# viloyat = input("viloyatini kiriting ")
 
 # print(kocha + ' kochasi,\n'  +  mahalla + ' mahallasi, \n' +  tuman + ' tumani \n' + viloyat + ' viloyati \n' )  
 
 
 # Yuqoridagi matnni f-string yordamida, yangi, manzil deb nomlangan o'zgaruvchiga yuklang
 
-yangi_manzil = f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati"
+# yangi_manzil = f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati"
 
 # manzilga biz yuqorida o'rgangan title(), upper(), lower() , capitalize() metodlarini qo'llab ko'ring.
-print(yangi_manzil.upper())
-print(yangi_manzil.lower())
-print(yangi_manzil.title())
-print(yangi_manzil.capitalize())
+# print(yangi_manzil.upper())
+# print(yangi_manzil.lower())
+# print(yangi_manzil.title())
+# print(yangi_manzil.capitalize())
 
 
+################# Sonlar
 
+# Foydalanuvchi kiritgan sonning kvadrati va kubini konsolga chiqaruvchi dastur
 
+# son_kirit = int(input('Son kiriting >>> '))
 
+# print(f'{son_kirit} ning kvadrati {son_kirit**2} ga teng')
+# print(f'{son_kirit} ning kubi {son_kirit**3} ga teng')
+
+# Foydalanuvchining yoshini so'rab, uning tug'ilgan yilini hisoblab, konsolga chiqaruvchi dastur
+
+# yosh = int(input('Yoshingizni kiriting: '))
+
+# print(f"Siz {2026-yosh} da tug'ilgansiz")
+
+# Foydalanuvchidan ikki son kiritshni so'rab, kiritilgan sonlarning yig'indisi, ayirmasi, ko'paytmasi va bo'linmasini chiqaruvchi dastur
+
+son1 = int(input('1-sonni kiriting: '))
+son2 = int(input('2-sonni kiriting: '))
+
+print(f'{son1} + {son2} = {son1+son2}')
+print(f'{son1} - {son2} = {son1-son2}')
+print(f'{son1} * {son2} = {son1*son2}')
+print(f'{son1} / {son2} = {son1/son2}')
