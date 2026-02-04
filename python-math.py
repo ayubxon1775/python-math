51a47a2060adc4661999e67d7408e32c2124a03e
Ayubxon Axmatxonov <114375261+ayubxon1775@users.noreply.github.com>
2026-02-04 14:36:27 +0500

    royxatlar bilan ishlash

--- python-math.py
+++ python-math.py
@@ -109,7 +109,7 @@
 
 # ismlar degan ro'yxat yarating va kamida 3 ta yaqin do'stingizning ismini kiriting
 
-ismlar = ['abdulbosit', 'akramjon', 'abdushohid', 'abdurrohman']
+# ismlar = ['abdulbosit', 'akramjon', 'abdushohid', 'abdurrohman']
 
 # Ro'yxatdagi har bir do'stingizga qisqa xabar yozib konsolga chiqaring: 
 
@@ -120,14 +120,14 @@ ismlar = ['abdulbosit', 'akramjon', 'abdushohid', 'abdurrohman']
 
 # sonlar deb nomlangan ro'yxat yarating va ichiga turli sonlarni yuklang (musbat, manfiy, butun, o'nlik). 
 
-sonlar = [15, 1, -5, 2.2, 2.0, -3]
+# sonlar = [15, 1, -5, 2.2, 2.0, -3]
 
 # Yuqoridagi ro'yxatdagi sonlar ustida turli arifmetik amallar bajarib ko'ring. Ro'yxatdagi
 # ba'zi sonlarning qiymatini o'zgartiring, ba'zilarini esa almashtiring. 
 
-sonlar[0] = 18
-sonlar[1] = 20
-sonlar[2] = sonlar[3]
+# sonlar[0] = 18
+# sonlar[1] = 20
+# sonlar[2] = sonlar[3]
 # print(f'{sonlar[0]*5}')
 # print(f'{sonlar[1]+2}')
 # print(f'{sonlar[2]*5}')
@@ -136,59 +136,156 @@ sonlar[2] = sonlar[3]
 # t_shaxslarva z_shaxslar degan 2 ta ro'yxat yarating va biriga o'zingiz eng ko'p hurmat qilgan
 #  tarixiy shaxslarning, ikkinchisiga esa zamonamizdagi tirik bo'lgan shaxslarning ismini kiriting. 
 
-t_shaxslar = ['abu bakr', 'umar ibn hattob', 'Ali', 'Abu Ubayda']
-z_shaxslar = ['Muhammad ali', 'Erdogan', 'Abror Muhtor ali'] 
+# t_shaxslar = ['abu bakr', 'umar ibn hattob', 'Ali', 'Abu Ubayda']
+# z_shaxslar = ['Muhammad ali', 'Erdogan', 'Abror Muhtor ali'] 
 
 
 # Yuqoridagi ro'yxatlarning har biridan bittadan qiymatni sug'urib olib (.pop()), quyidagi ko'rinishda chiqaring:
 
-t_shaxs = t_shaxslar.pop(0)
-z_shaxs = z_shaxslar.pop(2)
+# t_shaxs = t_shaxslar.pop(0)
+# z_shaxs = z_shaxslar.pop(2)
 
 # print(f"Men tarixiy shaxslardan {t_shaxs} bilan zamonaviy shaxslardan {z_shaxs} bilan suhbat qilishni istardim")
 
 # friends nomli bo'sh ro'yxat tuzing va unga .append() yordamida 5-6 ta mehmonga chaqirmoqchi
 #  bo'lgan do'stlaringizni kiriting. 
 
-friends = []
+# friends = []
 
-friends.append('Abdushohid')
-friends.append('Abdulbosit')
-friends.append('Akramjon')
-friends.append('Sobitxon')
-friends.append('Abdurrohman')
-print(friends)
+# friends.append('Abdushohid')
+# friends.append('Abdulbosit')
+# friends.append('Akramjon')
+# friends.append('Sobitxon')
+# friends.append('Abdurrohman')
+# print(friends)
 
 # Yuqoridagi ro'yxatdan mehmonga kela olmaydigan odamlarni .remove() metodi yordamida o'chrib tashlang.
 
-friends.remove('Abdurrohman') 
-friends.remove('Sobitxon') 
-print(friends)
+# friends.remove('Abdurrohman') 
+# friends.remove('Sobitxon') 
+# print(friends)
 
 # Ro'yxatning oxiriga, boshiga va o'rtasiga yangi ismlar qo'shing.
 
-friends.insert(0, 'Mutalli')
-friends.append('Asatbek')
-print(friends)
+# friends.insert(0, 'Mutalli')
+# friends.append('Asatbek')
+# print(friends)
 
 
 # Yangi mehmonlar deb nomlangan bo'sh ro'yxat yarating. .pop() va .append() metodlari yordamida
 #  mehmonga kelgan do'stlaringizning ismini friends ro'yxatidan sug'urib olib, mehmonlar ro'yxatiga qo'shing.
 
-Mehmonlar = []
+# Mehmonlar = []
 
-Mehmonlar.append(friends.pop(0))
-Mehmonlar.append(friends.pop(0))
-Mehmonlar.append(friends.pop(0))
-Mehmonlar.append(friends.pop(0))
-Mehmonlar.append(friends.pop(0))
+# Mehmonlar.append(friends.pop(0))
+# Mehmonlar.append(friends.pop(0))
+# Mehmonlar.append(friends.pop(0))
+# Mehmonlar.append(friends.pop(0))
+# Mehmonlar.append(friends.pop(0))
+
+# print(Mehmonlar)
+# print(f'Mehmonga kelganlar {Mehmonlar}')
+
+##############  Royxatlar bilar ishlash
+
+# O'zingizga ma'lum davlatlarning ro'yxatini tuzing va ro'yxatni konsolga chiqaring
+# davlatlar = ["O'zbekiston", "Qozog'iston", "Turkiya", "Qirg'iziston", "Kanada", "AQSH", "Britaniya"]
+
+# print(davlatlar)
+
+# Ro'yxatning uzunligini konsolga chiqaring
+
+# print(len(davlatlar))
+
+# sorted() funktsiyasi yordamida ro'yxatni tartiblangan holda konsolga chiqaring
+
+# tartib_davlatlar = sorted(davlatlar)
+# print(tartib_davlatlar)
+
+# sorted() yordamida ro'yxatni teskari tartibda konsolga chiqaring
+
+# tartib_davlatlar = sorted(davlatlar, reverse=True)
+# print(tartib_davlatlar)
+
+# Asl ro'yxatni qaytadan konsolga chiqaring
+
+# print(davlatlar)
+
+# reverse() metodi yordamida ro'yxatni ortidan boshlab chiqaring
+
+# davlatlar.reverse()
+# print(davlatlar)
+
+
+# sort() metodi yordamida ro'yxatni avval alifbo bo'yicha,
+#  keyin esa alifboga teskari tartibda konsolga chiqaring.
+
+# davlatlar.sort()
+# print(davlatlar)
+# davlatlar.sort(reverse=True)
+# print(davlatlar)
+
+
+# 120 dan 1200 gacha bo'lgan juft sonlar ro'yxatini tuzing
+
+# sonlar = list(range(120,1200, 2))
+# print(sonlar)
+
+# Ro'yxatdagi sonlar yig'indisini hisoblang va konsolga chiqaring
+
+# yigindi = sum(sonlar)
+# print(yigindi)
+
+# Ro'yxatdagi eng katta va eng kichik son o'rtasidagi ayirmani hisoblang va konsolga chiqaring
+
+# eng_katta = max(sonlar)
+# eng_kichik = min(sonlar)
+
+# print(f'{eng_katta}-{eng_kichik}={eng_katta-eng_kichik}')
+
+# Ro'yxatdagi elementlar sonini hisoblang
+
+# print(len(sonlar))
+
+
+# Ro'yxatning boshidan, o'rtasidan va oxiridan 20 ta qiymatni konsolga chiqaring
+
+# boshidan = sonlar[:20]
+# ortasidan = sonlar[530:550]
+# oxiridan = sonlar[-20:]
+
+# print(boshidan)
+# print(ortasidan)
+# print(oxiridan)
+
+# taomlar degan ro'yxat yarating va ichiga istalgan 5ta taomni kiriting
+
+# taomlar = ['osh', 'shashlik','manti', 'somsa', 'shorva']
+# print(taomlar)
+
+# nonushta degan yangi ro'yxatga taomlardan nusxa oling
+
+# nonushta = taomlar[:]
+# print(nonushta)
+
+# Yangi ro'yxatda faqat nonushtaga yeyiladigan taomlarni qoldiring, va qo'shimcha 2 ta taom qo'shing
+
+# nonushta.remove('somsa')
+# nonushta.remove('manti')
+# nonushta.remove('shashlik')
+# nonushta.append('mastava')
+# nonushta.append('qaymoq')
 
 
 
-print(Mehmonlar)
-print(f'Mehmonga kelganlar {Mehmonlar}')
+# Ikkala ro'yxatni ham (taomlar va nonushta) konsolga chiqaring
+# print(nonushta)
+# print(taomlar)
 
+# Yuqoridagi nonushta ro'yxatini o'zgarmas ro'yxatga aylantiring
+# va nonushta[0] = "qaymoq va non" deb qiymat berib ko'ring.
 
-
-
+# nonushta = tuple(nonushta)
+# nonushta[0] = 'qaymoq va non'
+# print(nonushta)
 
