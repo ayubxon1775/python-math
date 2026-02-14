a2966160282181df0384f19d40624f9e1c45480a
Ayubxon Axmatxonov <114375261+ayubxon1775@users.noreply.github.com>
2026-02-14 10:59:52 +0500

    while tsikli

--- python-math.py
+++ python-math.py
@@ -792,3 +792,62 @@ cars = ['toyoto', 'mazda', 'hyundai', 'gm', 'kia']
 #           f"\nPul birligi: {info['pul birligi']}")
 # else:
 #     print("Bizda bu davlat haqida ma'lumot mavjud emas")
+
+
+######### while tsikli
+
+
+# Foydalanuvchidan yaxshi ko'rgan kitoblarini kiritishni so'rang. 
+# Foydalanuvchi stop so'zini yozishi bilan dasturni to'xtating
+
+# savol = "O'zingiz yoqtirgan kitoblarni kiriting: "
+# savol += "(Dasturni to'xtatish uchun 'stop' so'zini yozing): "
+
+# qiymat = ''
+# while qiymat != 'stop':
+#     qiymat = input(savol)
+#     if qiymat != 'stop':
+#         print(qiymat)
+
+
+# Muzeyga chipta narhi foydalanuvchining yoshiga bog'liq: 7 dan yoshlarga - 2000 so'm, 7-18 gacha 3000 so'm,
+# 18-65 gacha 10000 so'm, 65 dan kattalarga bepul. Shunday while tsikl yozingki, 
+# dastur foydalanuvchi yoshini so'rasin va chipta narhini chiqarsin.
+# Foydalanuvchi exit yoki quit deb yozganda dastur to'xtasin (ikkita shartni ham tekshiring).
+
+# savol = 'yoshingizni kiriting: '
+# savol += 'Dasturni toxtatish uchun "exit" yoki "quit" tugmasini boshing '
+
+# while True:
+#     qiymat = input(savol)
+
+#     if qiymat == 'exit' or qiymat == 'quit':
+#         print('dastur tugadi.')
+#         break
+#     qiymat = int(qiymat)
+#     if qiymat <= 7:
+#         print('sizga kirish narxi 2000 som')
+#     elif  7 < qiymat < 18:
+#         print('sizga kirish narxi 3000 som')
+#     elif  18 <= qiymat <= 65:
+#         print('sizga kirish narxi 10000 som')
+#     else:
+#         print('sizga kirish bepul')
+    
+# Quyidagi dasturda bir nechta mantiqiy xatolar bor. 
+# Jumladan, xusisiy holatlarda tsikl abadiy qaytarilib qolmoqda. Xatolarni to'g'rilay olasizmi?
+
+
+# savol ="Kiritilgan sonning ildizini qaytaruvchi dastur.\n"
+# savol += "Musbat son kiriting "
+# savol += "(dasturni to'xtatish uchun 'exit' deb yozing): "
+
+# while True:
+#     qiymat = input(savol)
+#     if qiymat=='exit':
+#         break
+#     elif float(qiymat)<0:
+#         continue
+#     else:
+#         ildiz = float(qiymat)**(0.5)
+#         print(f"{qiymat} ning ildizi {ildiz} ga teng")
