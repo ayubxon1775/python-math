72d9874493b65f3924319a96b07745a1d41a2cb4
Ayubxon Axmatxonov <114375261+ayubxon1775@users.noreply.github.com>
2026-02-28 11:55:59 +0500

    modullar

--- python-math.py
+++ python-math.py
@@ -1328,3 +1328,56 @@ cars = ['toyoto', 'mazda', 'hyundai', 'gm', 'kia']
 
 # print(talaba1)
 # print(talaba2)
+
+######################
+
+
+# modullar
+
+# import avto_info_mod as aim
+
+# avto1 = aim.avto_info('GM', 'Malibu', 'Qora', 'Avtomat', 2020, 40000)
+# aim.info_print(avto1)
+
+# from avto_info_mod import info_print as aprint, avto_info as ainfo
+
+# avto1 = ainfo('GM', 'Malibu', 'Qora', 'Avtomat', 2020, 40000)
+# aprint(avto1)
+
+# from avto_info_mod import *
+
+# avto1 = avto_info("GM", "Malibu", "Qora", "avtomat", 2020,40000)
+# info_print(avto1)
+
+# import math
+
+# x = 4
+# y = 5
+# print(math.sqrt(x))
+# print(pow(x,y))
+
+# from math import pi
+# print(pi)
+
+# print(math.log2(8))
+# print(math.log10(100))
+
+# import random as r
+
+# son = r.randint(0,100)
+# print(son)
+
+# ismlar = ['olim', 'anvar', 'hasan', 'husan']
+# ism = r.choice(ismlar)
+# print(ism)
+# print(r.choice(ism))
+
+# x = list(range(0,51,5))
+# print(x)
+# print(r.choice(x))
+
+# x = list(range(11))
+# print(x)
+# r.shuffle(x)
+# print(x)
+
