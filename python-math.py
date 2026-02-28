19bf3f05b7e4ae48327cdc9d250b8c7238fed5cf
Ayubxon Axmatxonov <114375261+ayubxon1775@users.noreply.github.com>
2026-02-28 12:16:08 +0500

    lambda, funksiyalar

--- python-math.py
+++ python-math.py
@@ -1381,3 +1381,45 @@ cars = ['toyoto', 'mazda', 'hyundai', 'gm', 'kia']
 # r.shuffle(x)
 # print(x)
 
+###############
+
+# Funksiyalar va so'nggi soz
+
+# import math
+# uzunlik = lambda pi, r: 2*pi*r
+# print(uzunlik(math.pi, 10))
+
+# product = lambda x, y: x**y
+# print(product(2,3))
+
+# def daraja(n):
+#     return lambda x: x**n
+
+# kvadrat = daraja(2)
+# kub = daraja(3)
+# print(f'3-ning kvadrati {kvadrat(3)} ga, kubi {kub(3)} ga teng')
+
+# from math import sqrt
+# sonlar = list(range(11))
+# ildizlar = list(map(sqrt,sonlar))
+
+
+# sonlar = list(range(11))
+
+# def daraja2(x):
+#     return x*x
+
+# print(list(map(daraja2, sonlar)))
+
+# kvadratlar = list(map(lambda x: x*x, sonlar))
+# print(kvadratlar)
+
+# a = [4,5,6]
+# b = [7,8,9]
+# a_plus_b = list(map(lambda x, y: x + y, a, b))
+# print(a_plus_b)
+
+# ismlar = ['hasan', 'husan', 'olim', 'umid']
+# print(list(map(lambda matn: matn.upper(), ismlar)))
+
+
